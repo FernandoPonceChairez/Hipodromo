f5cf7390ce77930773716de8e0fa472a4b9d5cf2
FernandoPonceChairez <134646981+FernandoPonceChairez@users.noreply.github.com>
2023-08-07 18:42:32 -0600

    Hipodromo

---
+++ src/config.js
@@ -0,0 +1,13 @@
+// config.js
+
+module.exports = {
+    database: {
+      localhost: '127.0.0.1',
+      user: 'root',
+      password: '1234',
+      database: 'hipodromo2',
+    },
+
+    
+  };
+  
